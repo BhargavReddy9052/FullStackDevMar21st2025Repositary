c3c3441708f721f2da1ff948972b29829e20c469
BhargavReddy9052 <reddybhargav8739@gmail.com>
2025-05-02 13:23:23 +0530

    New Demo

--- AccessSpecifier/src/test/java/executiondemo/p3/test.java
+++
@@ -1,9 +0,0 @@
-package executiondemo.p3;
-
-public class test {
-    public static void main(String[] args) {
-        System.out.println("Hello Development");
-        System.out.println("hello world i am dev");
-
-    }
-}
---
+++ AccessSpecifier/src/test/java/p4/Test1.java
@@ -0,0 +1,9 @@
+package p4;
+
+public class Test1
+{
+    public static void main(String[] args) {
+
+        System.out.println("hi");
+    }
+}
